a894298337ca3305218d5c8aeeef3e39f2bc6fd2
markthos <aydenthos@gmail.com>
2025-07-05 13:15:18 +0100

    finished navbar skeleton

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,11 +3,11 @@ import Image from "next/image"
 export const NavBar = () => {
     return (
         <header>
-            <div className="navbar">
-                <div className="flex">
+            <div className="navbar w-full fixed top-0 left-0 z-50 bg-black text-white">
+                <div className="flex intems-center justify-between p-2 md:p-4">
                     <button className="p-2">Menu</button>
 
-                    <div className="absolute">
+                    <div className="absolute left-1/2 -translate-x-1/2 transform">
                         <Image
                             src="/logo.svg"
                             alt="Cote Royale Paris"
